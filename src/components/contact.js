61d3325f376f9aa97c20cddc882dc0ac4bf740d9
dineshdeveloper619 <dk4322350@gmail.com>
2022-08-17 13:32:04 +0530

    two

--- src/components/contact.js
+++ src/components/contact.js
@@ -5,7 +5,7 @@ const Contact=()=>{
       <div className="About-container">
         <h1 id="contact">Contact</h1>
         <p>You can contact me by gmail. Here i can feed my E-mail address : <a href="mailto:dk4322350@gmail.com">dk4322350@gmail.com</a>. 
-        contact me for website and editing freelancing works. </p>
+        contact me for website and editing works. </p>
         <h1 id="thank">Thank You For Visiting !</h1>
       </div>
     )
